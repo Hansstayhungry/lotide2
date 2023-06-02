2cf73ad963df21ca358afee87a44f8b05bc7fe6e
Hans Wang <hansmikaa@live.cn>
2023-06-02 01:20:30 -0400

    complete findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  let result = "🛑🛑🛑 Assertion Failed:";
+  if (actual === expected) {
+    result = "✅✅✅ Assertion Passed:";
+    console.log(`${result} ${actual} === ${expected}`);
+    return;
+  }
+
+  console.log(`${result} ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+// TEST
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
