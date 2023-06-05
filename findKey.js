acb80a748a9bc51e4699c1b70bc7e6d2d5b97dfa
Hans Wang <hansmikaa@live.cn>
2023-06-05 04:13:16 -0400

    implement findKey function using callback

---
+++ findKey.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  let result = "🛑🛑🛑 Assertion Failed:";
+  if (actual === expected) {
+    result = "✅✅✅ Assertion Passed:";
+    console.log(`${result} ${actual} === ${expected}`);
+    return;
+  }
+
+  console.log(`${result} ${actual} !== ${expected}`);
+};
+
+const findKey = function(object, callback) {
+  let names = [];
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return names;
+};
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
