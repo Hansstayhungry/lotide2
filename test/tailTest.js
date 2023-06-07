82c91c0faff7a239d56a0a13cdf2e6d8f7998009
Hans Wang <hansmikaa@live.cn>
2023-06-07 07:25:23 -0400

    use chai mocha for tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 // Tail array to pass in
 const tailResult = tail(["Hello", "Lighthouse", "Labs"]);
 
-// Test Case 1: Check the returned array elements
-assertEqual(tailResult.length, 2); // ensure we get back two elements
-assertEqual(tailResult[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(tailResult[1], "Las"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("ensure we get back two elements", () => {
+    assert.strictEqual(tailResult.length, 2);
+  });
 
-console.log("---");
+  it("ensure first element is 'Lighthouse'", () => {
+    assert.strictEqual(tailResult[0], "Lighthouse"); 
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("ensure second element is 'Labs'", () => {
+    assert.strictEqual(tailResult[1], "Labs"); 
+  });
+
+});
